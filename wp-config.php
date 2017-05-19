e2eb000299740ff82c0c29cca0a6f35569321902
Pulsar <2571440661@qq.com>
2017-05-19 17:38:16 +0800

    config

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 
 /** MySQL主机 */
-define('DB_HOST', '127.0.0.1:3306');
+define('DB_HOST', 'mysql');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8mb4');
